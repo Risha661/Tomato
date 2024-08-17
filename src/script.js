53d27e634297389330c116f373ebfa599c17b7af
unknown <git@github.com:Risha661/CRM.git>
2024-08-17 16:04:56 +0300

    Create Tomato counter + id + changeName

--- src/script.js
+++ src/script.js
@@ -1,3 +1,32 @@
 import "./script/main";
 
-import './scss/index.scss';
+import "./scss/index.scss";
+
+class Realize {
+    constructor(name, counter = 0) {
+        this.name = name;
+        this.counter = counter;
+        this.id = this.generateRandomId();
+    }
+
+    generateRandomId() {
+        return new Date().getTime().toString(16);
+    }
+
+    changeName(newName) {
+        this.name = newName;
+        return this.name;
+    }
+
+    changeCounter() {
+        this.counter += 1;
+        return this.counter;
+    }
+}
+
+const timer = new Realize("New timer");
+
+console.log(timer.generateRandomId());
+console.log(timer.id);
+console.log(timer.changeName("Новый таймер"));
+console.log(timer.changeCounter());
